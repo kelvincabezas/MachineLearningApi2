daff0f0a8196647d4e9516c1399190be230e8b92
kelvincabezas <ruben.cabezas@outlook.com>
2024-11-21 21:58:13 -0500

    ajuste

--- API_IC.py
+++ API_IC.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import requests
-from supabse import create_client,Client
+from supabase import create_client,Client
 
 SUPABSE_URL="https://nirofruznluznvcrenzq.supabase.co"
 SUPABASE_KEY ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5pcm9mcnV6bmx1em52Y3JlbnpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYzNTk5NzIsImV4cCI6MjA0MTkzNTk3Mn0.8DxdMyiyv-J60FUraexHkiA_4YWKdzTvYBconoHmoOE"
-supabse: Client = create_client(SUPABSE_URL,SUPABASE_KEY)
+supabase: Client = create_client(SUPABSE_URL,SUPABASE_KEY)
 
 def get_exchange_rate(base_currency ="USD",target_currency ="EUR"):
     url =f"https://api.exchangerate.host/latest?base={base_currency}&symbols={target_currency}"
@@ -15,7 +15,7 @@ def get_exchange_rate(base_currency ="USD",target_currency ="EUR"):
         return{"error":"No se puedo conectar con el api"}    
 
 def save_to_supase(data):
-    response = supabse.table("exchange_rates").insert(data).execute()
+    response = supabase.table("exchange_rates").insert(data).execute()
     return response
 
 st.title("Consulta y registro de tipo de cambio")
