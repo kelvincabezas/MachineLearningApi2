c5eaa1074cddd60b660b899ab16d8d5ed75d143b
kelvincabezas <ruben.cabezas@outlook.com>
2024-11-21 21:41:03 -0500

    o

--- API_IC.py
+++ API_IC.py
@@ -6,7 +6,6 @@ SUPABSE_URL="https://nirofruznluznvcrenzq.supabase.co"
 SUPABASE_KEY ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5pcm9mcnV6bmx1em52Y3JlbnpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYzNTk5NzIsImV4cCI6MjA0MTkzNTk3Mn0.8DxdMyiyv-J60FUraexHkiA_4YWKdzTvYBconoHmoOE"
 supabse: Client = create_client(SUPABSE_URL,SUPABASE_KEY)
 
-
 def get_exchange_rate(base_currency ="USD",target_currency ="EUR"):
     url =f"https://api.exchangerate.host/latest?base={base_currency}&symbols={target_currency}"
     response= requests.get(url)
